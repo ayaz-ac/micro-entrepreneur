07a5c7a047adb46413bad31feed3f7e065287269
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-02-28 22:05:27 +0100

    Remove language scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope '(:locale)', locale: /en|fr/ do
-    get '/:locale' => 'pages#home'
-    root 'pages#home'
+  root 'pages#home'
 
-    get 'privacy', to: 'pages#privacy'
-    get 'terms', to: 'pages#terms'
+  get 'privacy', to: 'pages#privacy'
+  get 'terms', to: 'pages#terms'
 
-    devise_for :users
-  end
+  devise_for :users
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
