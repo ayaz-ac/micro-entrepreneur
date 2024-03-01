b44fadf24e2b8677e518ffe3dd9b87283aaa4284
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-03-01 16:12:46 +0100

    Add basic monthly calendar for ActivtyReport

--- app/controllers/activity_reports_controller.rb
+++ app/controllers/activity_reports_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ActivityReportsController < ApplicationController
-  def show; end
+  def show
+    @date = Date.parse(params.fetch(:date, Time.zone.today.to_s)).at_beginning_of_month
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def month_offset(date)
+    date.wday - 1
+  end
 end
