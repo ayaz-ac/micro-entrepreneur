47b6215b44f85515bed2da7bff837dfcb4594f75
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-03-01 18:00:23 +0100

    Add style to the calendar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,14 @@
 
 module ApplicationHelper
   def month_offset(date)
+    return 6 if date.wday.zero?
+
     date.wday - 1
+  end
+
+  def month_inset(date)
+    return 0 if date.end_of_month.wday.zero?
+
+    7 - date.end_of_month.wday
   end
 end
