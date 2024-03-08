8b8d7e2dad56a077afed5f8cac54b1417df5e099
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-03-08 12:39:46 +0100

    Optimize code linked to details

--- app/models/activity_report.rb
+++ app/models/activity_report.rb
@@ -8,6 +8,16 @@ class ActivityReport < ApplicationRecord
   validates :start_date, uniqueness: { scope: %i[end_date user_id] }
   validate :dates_in_same_month_and_year
 
+  def total_worked_days
+    details['total_worked_days'] || 0
+  end
+
+  def estimated_income
+    details['estimated_income'] || 0
+  end
+
+  private
+
   def dates_in_same_month_and_year
     return unless start_date.present? && end_date.present?
 
--- app/models/concerns/activity_report_details.rb
+++ app/models/concerns/activity_report_details.rb
@@ -7,50 +7,58 @@ module ActivityReportDetails
     before_create :initialize_details
   end
 
-  def total_worked_days
-    details['total_worked_days']
-  end
-
-  def update_off_days_in_details
+  def update_days_status_in_details
     details['days'].each do |day|
-      day_name = Date.parse(day['date']).strftime('%A').downcase
-
-      if off_days.any? day_name
+      if off_day?(day['date'])
         day['status'] = 'off'
       elsif day['status'] == 'off'
         day['status'] = 'full'
       end
     end
 
-    count_total_worked_days
-
+    update_extras
     save!
   end
 
   private
 
   def initialize_details
-    self.details = { 'days' => [], 'total_worked_days' => 0 }
+    self.details = { 'days' => [], 'total_worked_days' => 0, 'estimated_income' => 0 }
 
     (start_date.to_date..end_date.to_date).each do |day|
       details['days'] << {
         'date' => day,
-        'status' => day_off?(off_days, day) ? 'off' : 'full'
+        'status' => off_day?(day) ? 'off' : 'full'
       }
     end
 
-    count_total_worked_days
+    update_extras
   end
 
-  def day_off?(off_days, day)
-    off_days.any? day.strftime('%A').downcase
+  def off_day?(day)
+    off_days.include?(lowercase_weekday(day))
+  end
+
+  def update_extras
+    count_total_worked_days
+    calculate_estimated_income
   end
 
   def count_total_worked_days
     details['total_worked_days'] = details['days'].count { |day| day['status'] != 'off' }
   end
 
+  def calculate_estimated_income
+    # TODO: Call URSSAF Api
+    details['estimated_income'] = 400 * total_worked_days
+  end
+
   def off_days
     @off_days ||= user.configured_off_days_of_week
+  end
+
+  def lowercase_weekday(date)
+    date = Date.parse(date) if date.is_a? String
+    date.strftime('%A').downcase
   end
 end
