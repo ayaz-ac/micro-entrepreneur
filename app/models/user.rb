14668a11d878d25ad356e21d0b8e66ecac566420
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-03-12 09:41:18 +0100

    Copy average_daily_rate to ActivityReport from the User model when updating

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,15 @@ class User < ApplicationRecord
   has_many :configured_off_days, dependent: :destroy
   has_many :work_days, dependent: :destroy
 
+  after_update :copy_average_daily_rate_to_activity_reports, if: :average_daily_rate_changed?
+
   def configured_off_days_of_week
     configured_off_days.map(&:day_of_week)
+  end
+
+  private
+
+  def copy_average_daily_rate_to_activity_reports
+    activity_reports.from_this_month.update_all(average_daily_rate:) # rubocop:disable Rails/SkipsModelValidations
   end
 end
