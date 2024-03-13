7cee175b1a4dd08965b402d38ce1b38f0faf5176
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-03-13 07:03:49 +0100

    Inverse assert_equal in UserTest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,7 @@ class UserTest < ActiveSupport::TestCase
   test 'it should create a user with default values' do
     @user = User.create!(email: 'test@example.com', password: 'password')
 
-    assert_equal @user.average_daily_rate, 0
-    assert_equal @user.configured_off_days.map(&:day_of_week), %w[saturday sunday]
+    assert_equal 0, @user.average_daily_rate
+    assert_equal %w[saturday sunday], @user.configured_off_days.map(&:day_of_week)
   end
 end
