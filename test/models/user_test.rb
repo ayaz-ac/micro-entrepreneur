46aec8918cabc59b01b0bcf2783df09034989b16
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-03-13 06:54:28 +0100

    Update UserTest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'it should create a user with default values' do
+    @user = User.create!(email: 'test@example.com', password: 'password')
+
+    assert_equal @user.average_daily_rate, 0
+    assert_equal @user.configured_off_days.map(&:day_of_week), %w[saturday sunday]
+  end
 end
