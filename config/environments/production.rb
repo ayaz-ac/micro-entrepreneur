43cc51c72a812e14041ec197e85464d8d123b3dd
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-03-15 17:05:40 +0100

    Fix db name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "saas_template_production"
+  # config.active_job.queue_name_prefix = "micro_entrepreneur_production"
 
   config.action_mailer.perform_caching = false
 
