ea9b3b042fdd79f35e4206186b994ba43e9c1e03
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-05-10 01:04:30 +0200

    Fix SignUpFlowsTest

--- test/integration/sign_up_flows_test.rb
+++ test/integration/sign_up_flows_test.rb
@@ -4,14 +4,14 @@ require 'test_helper'
 
 class SignUpFlowsTest < ActionDispatch::IntegrationTest
   test 'it should not create a user without the average daily rate' do
-    post user_registration_path, params: { user: { email: 'test@example.com', password: 'password', password_confirmation: 'password' } }
+    post user_registration_path, params: { user: user_params(nil) }
 
     assert_response :unprocessable_entity
     assert_includes response.body, 'TJM doit être rempli(e)'
   end
 
   test 'it should not create a user with a invalid average daily rate' do
-    post user_registration_path, params: { user: { email: 'test@example.com', password: 'password', password_confirmation: 'password', average_daily_rate: 'abc' } }
+    post user_registration_path, params: { user: user_params('abc') }
 
     assert_response :unprocessable_entity
     assert_includes response.body, 'TJM n&#39;est pas un nombre'
@@ -19,18 +19,16 @@ class SignUpFlowsTest < ActionDispatch::IntegrationTest
 
   test "it should not sign up user if the average daily rate is below #{User::AVERAGE_DAILY_RATE_LIMIT}" do
     assert_no_changes -> { User.all.size } do
-      post user_registration_path,
-           params: { user: { email: 'user.test@example.com', password: 'password', password_confirmation: 'password',
-                             average_daily_rate: 50 } }
+      post user_registration_path, params: { user: user_params(50) }
 
       assert_response :unprocessable_entity
       assert_includes response.body, 'une erreur a empêché ce (cet ou cette) utilisateur d’être enregistré(e)'
-      assert_includes response.body, 'TJM doit être supérieur ou égal à 100' 
+      assert_includes response.body, 'TJM doit être supérieur ou égal à 100'
     end
   end
 
   test 'it should sign up user' do
-    post user_registration_path, params: { user: { email: 'test@example.com', password: 'password', password_confirmation: 'password', average_daily_rate: 150 } }
+    post user_registration_path, params: { user: user_params }
 
     assert_response :redirect
     follow_redirect!
@@ -38,5 +36,19 @@ class SignUpFlowsTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'div', 'Bienvenue ! Vous vous êtes bien enregistré(e).'
     assert_select 'a[href=?]', root_path
+  end
+
+  private
+
+  def user_params(average_daily_rate = 400)
+    {
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'password',
+      revenues_attributes: { 0 => { amount: '35000' } },
+      average_daily_rate:
+    }
   end
 end
