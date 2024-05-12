29ef258dede4b9edd683c15d71c95dbef900e0f9
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-05-12 14:22:41 +0200

    Check in configured days doesn't update previous ActivityReport

--- test/controllers/configured_off_days_controller_test.rb
+++ test/controllers/configured_off_days_controller_test.rb
@@ -12,22 +12,39 @@ class ConfiguredOffDaysControllerTest < ActionDispatch::IntegrationTest
     @user.update!(average_daily_rate: 500)
   end
 
-  test 'it should update configured off days' do
-    existing_configured_days = @user.configured_off_days_of_week
-    new_configured_days = %w[monday tuesday] << existing_configured_days.first
+  test 'it should update the configured off days of the future months' do
+    @activity_report = @user.activity_reports.find_by(start_date: Time.zone.today.beginning_of_month)
 
-    assert_changes -> { @user.configured_off_days.count } do
+    existing_configured_days = @user.configured_off_days_of_week
+    new_configured_days = %w[monday tuesday]
+
+    assert_difference -> { @user.configured_off_days.count }, new_configured_days.size do
       patch configured_off_days_url(params: {
                                       user: {
-                                        activity_report_id: @user.activity_reports.first.id,
-                                        configured_off_days: new_configured_days
+                                        activity_report_id: @activity_report.id,
+                                        configured_off_days: new_configured_days + existing_configured_days
                                       }
                                     }, format: :turbo_stream)
       assert_response :success
 
       @user.reload
 
-      assert new_configured_days.sort == @user.configured_off_days_of_week.sort
+      assert (new_configured_days + existing_configured_days).sort == @user.configured_off_days_of_week.sort
+    end
+
+    # Check if future months are updated
+    @user.activity_reports.from_this_month.find_each do |activity_report|
+      activity_report.days.each do |day|
+        assert_equal 'off', day['status'], 'Day is supposed to be off' if off_day?(day['date'], new_configured_days)
+      end
+    end
+
+    # Check if past months arent' update
+    @user.activity_reports.before_this_month.find_each do |activity_report|
+      activity_report.days.each do |day|
+        assert_not_equal 'off', day['status'], "Day isn't supposed to be off" if off_day?(day['date'],
+                                                                                          new_configured_days)
+      end
     end
   end
 
@@ -61,5 +78,11 @@ class ConfiguredOffDaysControllerTest < ActionDispatch::IntegrationTest
 
       @user.reload
     end
+  end
+
+  private
+
+  def off_day?(date, configured_off_days)
+    configured_off_days.include?(Date.parse(date).strftime('%A').downcase)
   end
 end
