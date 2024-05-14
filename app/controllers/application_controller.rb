afed2291db814a5023936e0052aa6d58ac12eacd
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-05-14 21:50:20 +0200

    Redirect user to checkout he didn't pay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :redirect_unless_premium, if: :user_signed_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,5 +9,31 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :average_daily_rate,
                                                        { revenues_attributes: [:amount] }])
+  end
+
+  private
+
+  def redirect_unless_premium
+    return if current_user.premium?
+
+    session = create_stripe_checkout_session
+
+    redirect_to session.url, allow_other_host: true
+  end
+
+  def create_stripe_checkout_session
+    Stripe::Checkout::Session.create(
+      {
+        line_items: [{ price: stripe_price_id, quantity: 1 }],
+        mode: 'payment',
+        customer_email: current_user.email,
+        success_url: root_url,
+        cancel_url: root_url
+      }
+    )
+  end
+
+  def stripe_price_id
+    Rails.application.credentials[Rails.env.to_sym][:stripe][:price_id]
   end
 end
