3518d40b6e0185454a23680f10e8e9fd63229765
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-05-15 17:42:17 +0200

    Remove Stripe status verification from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :redirect_unless_premium, if: :user_signed_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,31 +8,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :average_daily_rate,
                                                        { revenues_attributes: [:amount] }])
-  end
-
-  private
-
-  def redirect_unless_premium
-    return if current_user.premium?
-
-    session = create_stripe_checkout_session
-
-    redirect_to session.url, allow_other_host: true
-  end
-
-  def create_stripe_checkout_session
-    Stripe::Checkout::Session.create(
-      {
-        line_items: [{ price: stripe_price_id, quantity: 1 }],
-        mode: 'payment',
-        customer_email: current_user.email,
-        success_url: root_url,
-        cancel_url: root_url
-      }
-    )
-  end
-
-  def stripe_price_id
-    Rails.application.credentials[Rails.env.to_sym][:stripe][:price_id]
   end
 end
