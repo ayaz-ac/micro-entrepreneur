b034136aaf5520b63f07d0a179146fc3d41b633d
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2025-12-28 10:08:20 +0100

    Updated tailwind config for v4

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+import forms from '@tailwindcss/forms';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+import typography from '@tailwindcss/typography';
+import containerQueries from '@tailwindcss/container-queries';
+
+export default {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
@@ -6,40 +11,10 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}',
     './app/helpers/tailwind_builder.rb'
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        logo: ['Poppins', 'sans-serif'],
-        display: ['Bricolage Grotesque', 'sans-serif'],
-        body: ['Nunito Sans', 'sans-serif']
-      },
-      colors: {
-        primary: '#006D77',
-        secondary: '#3bbdc6',
-        'brand-50': '#F0F7FE',
-        'brand-100': '#DDEDFC',
-        'brand-200': '#C3E0FA',
-        'brand-300': '#99CDF7',
-        'brand-400': '#69B2F1',
-        'brand-500': '#4594EC',
-        'brand-600': '#3076E0',
-        'brand-700': '#2762CE',
-        'brand-800': '#264FA7',
-        'brand-900': '#244584',
-        'brand-950': '#14213D',
-        gray: '#F3F4F6',
-        black: '#030712',
-        'off-white': '#FAFAFA'
-      },
-      gridTemplateRows: {
-        'fit-content': 'minmax(0, 1fr)'
-      }
-    },
-  },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
+    forms,
+    aspectRatio,
+    typography,
+    containerQueries,
   ]
 }
