5654256047deec97ce925e0043f26a720a87b474
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2025-12-29 11:15:08 +0100

    Add user dropdown menu

--- app/javascript/controllers/switch_class_controller.js
+++ app/javascript/controllers/switch_class_controller.js
@@ -15,4 +15,19 @@ export default class extends Controller {
       }
     });
   }
+
+  hide(event) {
+    if (!this.element.contains(event.target)) {
+      this.elementTargets.forEach((element) => {
+        if (element.dataset.activeClass && element.dataset.inactiveClass) {
+          element.dataset.activeClass.split(' ').forEach((activeClass) => {
+            element.classList.remove(activeClass);
+          });
+          element.dataset.inactiveClass.split(' ').forEach((inactiveClass) => {
+            element.classList.add(inactiveClass);
+          });
+        }
+      });
+    }
+  }
 }
