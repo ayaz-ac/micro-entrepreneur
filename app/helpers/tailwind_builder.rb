7da51626aec53b7ddb3648be8b11175bf5dbc6be
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2025-12-31 20:11:16 +0100

    Downgraded Tailwind to v3 + updated builder

--- app/helpers/tailwind_builder.rb
+++ app/helpers/tailwind_builder.rb
@@ -2,15 +2,39 @@
 
 class TailwindBuilder < ActionView::Helpers::FormBuilder
   def label(method, text = nil, options = {})
-    super(method, text, options.reverse_merge({ class: 'text-brand-900' }))
+    if text.is_a?(Hash)
+      options = text
+      text = nil
+    end
+
+    options[:class] = [options[:class], 'block text-gray-900 mb-1 md:mb-0'].compact.join(' ')
+    super
   end
 
-  %i[text_field email_field password_field number_field].each do |field_type|
+  %i[datetime_field date_field text_field email_field password_field number_field].each do |field_type|
     define_method(field_type) do |method, options = {}|
-      basic_classes = 'mt-1.5 block w-full rounded-md border-brand-950'
-
-      super(method, options.reverse_merge({ class: add_error_classes(basic_classes, method) }))
+      options.reverse_merge!({ class: field_css_classes(method) })
+      field_html = super(method, options)
+      add_error_message_if_needed(field_html, method)
     end
+  end
+
+  def file_field(method, options = {})
+    options = options.reverse_merge!({ class: field_css_classes(method) })
+    field_html = super
+    add_error_message_if_needed(field_html, method)
+  end
+
+  def select(method, choices = nil, options = {}, html_options = {}, &)
+    html_options[:class] = [html_options[:class], field_css_classes(method)].compact.join(' ')
+    field_html = super
+    add_error_message_if_needed(field_html, method)
+  end
+
+  def time_select(method, options = {}, html_options = {}, &)
+    html_options[:class] = [html_options[:class], field_css_classes(method)].compact.join(' ')
+    field_html = super
+    add_error_message_if_needed(field_html, method)
   end
 
   def check_box(method, options = {})
@@ -18,18 +42,27 @@ class TailwindBuilder < ActionView::Helpers::FormBuilder
   end
 
   def submit(method, options = {})
-    super(method, options.reverse_merge({
-                                          class: 'text-center text-white rounded-lg bg-brand-500 px-8 py-2.5
-                                                  lg:transition lg:text-lg lg:hover:-translate-y-1 lg:hover:scale-105
-                                                  lg:hover:drop-shadow-lg duration-300'
-                                        }))
+    super(method, options.reverse_merge(
+      { class: 'btn-cta' }
+    ))
   end
 
   private
 
-  def add_error_classes(basic_classes, method)
-    return basic_classes if @object.nil? || @object.errors.exclude?(method)
+  def field_css_classes(method)
+    base_classes = 'block w-full rounded-md focus:border-2 focus:ring-0 focus:outline-none'
+    if object.errors[method].any?
+      "#{base_classes} border-red-500"
+    else
+      "#{base_classes} border-gray-300 focus:border-brand-500"
+    end
+  end
 
-    "#{basic_classes} border-2 border-red-500"
+  def add_error_message_if_needed(field_html, method)
+    return field_html unless object.errors[method].any?
+
+    error_message = @template.content_tag(:div, object.errors[method].first,
+                                          class: 'text-red-600 text-sm mt-1')
+    (field_html + error_message).html_safe # rubocop:disable Rails/OutputSafety
   end
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,9 +1,6 @@
-import forms from '@tailwindcss/forms';
-import aspectRatio from '@tailwindcss/aspect-ratio';
-import typography from '@tailwindcss/typography';
-import containerQueries from '@tailwindcss/container-queries';
+const defaultTheme = require('tailwindcss/defaultTheme')
 
-export default {
+module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
@@ -11,10 +8,35 @@ export default {
     './app/views/**/*.{erb,haml,html,slim}',
     './app/helpers/tailwind_builder.rb'
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        logo: ['Poppins', 'sans-serif'],
+        display: ['Bricolage Grotesque', 'sans-serif'],
+        body: ['Nunito Sans', 'sans-serif']
+      },
+      colors: {
+        'brand-50': '#f3f5f9',
+        'brand-100': '#e7ebf2',
+        'brand-200': '#d1d8e4',
+        'brand-300': '#a5b2c7',
+        'brand-400': '#788aa8',
+        'brand-500': '#556a8b',
+        'brand-600': '#3e506f',
+        'brand-700': '#2e3d57',
+        'brand-800': '#263349',
+        'brand-900': '#1c2739',
+        'brand-950': '#121b29'
+      },
+      gridTemplateRows: {
+        'fit-content': 'minmax(0, 1fr)'
+      }
+    },
+  },
   plugins: [
-    forms,
-    aspectRatio,
-    typography,
-    containerQueries,
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
   ]
 }
